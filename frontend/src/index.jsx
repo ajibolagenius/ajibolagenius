7d6722438177f570eaa6ac34ccd87ba696370b21
AJIBOLA (DON_GENIUS) AKELEBE <ajiboladolapogenius@gmail.com>
2026-05-20 10:23:30 +0100

    style: update default theme to dark in ThemeProvider

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,9 +6,9 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="light" storageKey="don-genius-theme" enableSystem>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider attribute="class" defaultTheme="dark" storageKey="don-genius-theme" enableSystem>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
 );
